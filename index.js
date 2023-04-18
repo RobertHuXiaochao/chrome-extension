52b371c77f6f5d650bfd78f77eeb6b5df8e7bc84
Robert Hu <realroberthu@outlook.com>
2023-04-18 21:55:18 +0800

    keep learning
    guess the expressions

--- index.js
+++ index.js
@@ -1,35 +1,33 @@
 let myLeads = [];
+
 const inputEl = document.getElementById("input-el");
-console.log(inputEl);
 
 const inputBtn = document.getElementById("input-btn");
-console.log(inputBtn);
 
 const ulEl = document.getElementById("ul-el");
-console.log(ulEl);
+
+localStorage.clear();
+let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
+console.log(leadsFromLocalStorage);
 
 inputBtn.addEventListener("click", function () {
-	console.log("Button clicked!");
 	myLeads.push(inputEl.value);
-	console.log(myLeads);
 	inputEl.value = "";
+	localStorage.setItem("myLeads", JSON.stringify(myLeads));
 	renderLeads();
 });
 
 function renderLeads() {
 	let listItems = "";
-	console.log(listItems);
-
 	for (let i = 0; i < myLeads.length; i++) {
 		console.log(myLeads[i]);
-		listItems += "<li>" + myLeads[i] + "</li>";
-		console.log(listItems);
-		// ulEl.innerHTML += "<li>" + myLeads[i] + "</li>";
-		// const li = document.createElement("li");
-		// li.textContent = myLeads[i];
-		// ulEl.append(li);
+		listItems += `
+			<li>
+				<a target="_blank" href="${myLeads[i]}">
+					${myLeads[i]}
+				</a>
+			</li>
+		`;
 	}
-
 	ulEl.innerHTML = listItems;
-	console.log(ulEl.innerHTML);
 }
